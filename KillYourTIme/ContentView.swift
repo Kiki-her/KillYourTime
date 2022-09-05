08642e15b2127f309279c8962d5b6dc18a52bdf0
kiki <k.kazaneko@gmail.com>
2022-09-05 09:49:26 +0900

    Initial Commit

---
+++ KillYourTIme/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KillYourTIme
+//
+//  Created by 久島風音 on 2022/09/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KillYourTIme/KillYourTImeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KillYourTImeApp.swift
+//  KillYourTIme
+//
+//  Created by 久島風音 on 2022/09/05.
+//
+
+import SwiftUI
+
+@main
+struct KillYourTImeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
